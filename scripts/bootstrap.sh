4cc84dcddca326ff7ec01c87749cdb00236c032b
pkgdemon <pkgdemonteam@gmail.com>
2024-01-02 00:15:44 -0500

    Additional checks for root

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: This script requires root privileges. Please run with 'sudo'."
+    exit 1
+fi
+
 source ../config/build.conf
 
 if [ ! -d "${WORKDIR}" ] ; then mkdir ${WORKDIR} ; fi
--- scripts/buildimage.sh
+++ scripts/buildimage.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check for root privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: This script requires root privileges. Please run with 'sudo'."
+    exit 1
+fi
+
 source ../config/build.conf
 
 mkarchiso -v -w ${ISO} -o ${WORKDIR} ${RELENG}
