2ae82510cc68948cc1e4a44f844667a64eb43613
pkgdemon <pkgdemonteam@gmail.com>
2024-01-05 03:04:41 -0500

    Fix booting after install to disk

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -118,12 +118,33 @@ genfstab -t PARTUUID "${MNT}" \
 | grep -v swap \
 | sed "s|vfat.*rw|vfat rw,x-systemd.idle-timeout=1min,x-systemd.automount,noauto,nofail|" \
 > "${MNT}"/etc/fstab
+sed -i '/zroot/d' "${MNT}"/etc/fstab
 
 # Copy files to new system
 rsync -a /etc/hostid "${MNT}"/etc/hostid
 
 # Sync vmlinuz needed for mkinitcpio
 rsync -a /run/archiso/bootmnt/arch/boot/x86_64/ "${MNT}"/boot/
+
+# Generate proper preset for installed system
+rm "${MNT}"/etc/mkinitcpio.conf.d/archiso.conf
+echo "# mkinitcpio preset file for the 'linux-lts' package
+
+#ALL_config=\"/etc/mkinitcpio.conf\"
+ALL_kver=\"/boot/vmlinuz-linux-lts\"
+ALL_microcode=(/boot/*-ucode.img)
+
+PRESETS=('default' 'fallback')
+
+#default_config=\"/etc/mkinitcpio.conf\"
+default_image=\"/boot/initramfs-linux-lts.img\"
+#default_uki=\"/efi/EFI/Linux/arch-linux-lts.efi\"
+#default_options=\"--splash /usr/share/systemd/bootctl/splash-arch.bmp\"
+
+#fallback_config=\"/etc/mkinitcpio.conf\"
+fallback_image=\"/boot/initramfs-linux-lts-fallback.img\"
+#fallback_uki=\"/efi/EFI/Linux/arch-linux-lts-fallback.efi\"
+fallback_options=\"-S autodetect\"" > "${MNT}"/etc/mkinitcpio.d/linux-lts.preset
 
 # Hardcode the timezone for now
 chroot "${MNT}" ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
