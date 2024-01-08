27dd5529a87e8dd1ddd7a9cf653eb2671694cda0
pkgdemon <pkgdemonteam@gmail.com>
2024-01-08 01:14:34 -0500

    Remove lines that should not be needed

--- installer/backend.py
+++ installer/backend.py
@@ -89,10 +89,6 @@ subprocess.run(["mount", "-t", "efivarfs", "none", os.path.join(MNT, "sys/firmwa
 
 # Generate fstab
 subprocess.run(["genfstab", "-t", "PARTUUID", MNT], capture_output=True)
-output = subprocess.run(["grep", "-v", "swap"], stdout=subprocess.PIPE, text=True, input=subprocess.PIPE.encode())
-output = subprocess.run(["sed", "s|vfat.*rw|vfat rw,x-systemd.idle-timeout=1min,x-systemd.automount,noauto,nofail|"], capture_output=True, text=True, input=output.stdout)
-with open(os.path.join(MNT, "etc/fstab"), "w") as f:
-    f.write(output.stdout)
 subprocess.run(["sed", "-i", "/zroot/d", os.path.join(MNT, "etc/fstab")])
 
 # Copy files to new system
