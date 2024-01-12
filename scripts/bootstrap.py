d4cea2e156b7014dc0fae473b1f7a3b49990b03d
pkgdemon <pkgdemonteam@gmail.com>
2024-01-11 23:36:57 -0500

    Try a different approach to remove linux without dash

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -42,14 +42,15 @@ def lts():
 
     # Remove packages that pull in linux from packages.x86_64
     file_path = os.path.join(RELENG, "packages.x86_64")
-    with open(file_path, "r+", encoding="utf-8") as f:
+    # Read the file content into a list
+    with open(file_path, "r", encoding="utf-8") as f:
         lines = f.readlines()
-    f.seek(0)
-    for line in lines:
-        if "linux" in line and line.strip() == "linux":
-            continue
-        f.write(line)
-    f.truncate()
+    # Open the file again for writing
+    with open(file_path, "w", encoding="utf-8") as f:
+        for line in lines:
+            if "linux" in line and line.strip() == "linux":
+                continue
+            f.write(line)
     for line in lines:
         if not line.startswith(("broadcom-wl", "b43-fwcutter")):
             f.write(line)
